90f2e9458ff400390e74548746d3f5414438f321
Andrey Moiseev <a.moiseev@kryptex.org>
2026-03-30 18:36:43 +0400

    Version 0.0.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import Extension, setup
 
 setup(name='kawpow',
-    version='0.0.9',
+    version='0.0.10',
     options={
         'bdist_wheel': {'py_limited_api': 'cp39'}
     },
